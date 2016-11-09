44306b2313e451f7399ec12bcddc29559a340484
Steven Pilkington <thePyro_13@gmx.com>
2016-11-10 00:45:15 +1100

    fixed zlibvc build

--- contrib/masmx64/bld_ml64.bat
+++ contrib/masmx64/bld_ml64.bat
@@ -1,2 +1,2 @@
-ml64.exe /Flinffasx64 /c /Zi inffasx64.asm
-ml64.exe /Flgvmat64   /c /Zi gvmat64.asm
+ml64.exe /safeseh /Flinffasx64 /c /Zi inffasx64.asm
+ml64.exe /safeseh /Flgvmat64   /c /Zi gvmat64.asm
